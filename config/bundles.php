e8ce93544c1792d9014d3163b590f7899ead01f1
on99833 <on99833@outlook.fr>
2023-04-20 13:36:34 +0200

    save

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,20 +9,20 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
-    {
-         // get the login error if there is one
-         $error = $authenticationUtils->getLastAuthenticationError();
+    // #[Route('/loginTest', name: 'app_login')]
+    // public function index(AuthenticationUtils $authenticationUtils): Response
+    // {
+    //      // get the login error if there is one
+    //      $error = $authenticationUtils->getLastAuthenticationError();
 
-         // last username entered by the user
-         $lastUsername = $authenticationUtils->getLastUsername();
+    //      // last username entered by the user
+    //      $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/index.html.twig', [
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ]);
-    }
+    //     return $this->render('login/index.html.twig', [
+    //         'last_username' => $lastUsername,
+    //         'error'         => $error,
+    //     ]);
+    // }
 
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout()
